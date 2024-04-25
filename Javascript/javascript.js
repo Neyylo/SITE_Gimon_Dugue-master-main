157cd2afb1bbd5251b7f02cd9b498b9da0d0c320
Nolan Dugué <nolan.dugue@gmail.com>
2024-04-25 16:26:14 +0200

    xcwc

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -1,4 +1,10 @@
-// Ici on a programmé l'heure.
+//Nolan Dugué et Elliot Gimon
+
+
+
+//Nolan
+
+//Ici on a programmé l'heure en faisant en sorte qu'il s'actualise seul sur le site.
 function addZero(i) {
     if (i < 10) {i = "0" + i}
     return i;
@@ -15,22 +21,22 @@ function addZero(i) {
     requestAnimationFrame(updateTime);
   };
 
-
-// Fonction pour changer le style
+//Nolan
+//Fonction pour changer le style
 function changerStyle() {
-  // Récupération de la feuille de style
+  //Récupération de la feuille de style
   var styleFeuille = document.getElementById('style-feuille');
-  // Vérifier l'état du style actuel
+  
   if (styleFeuille.getAttribute('href') === '../Styles/style.css') {
-      // Si le style actuel est le premier, changer vers le deuxième
       styleFeuille.setAttribute('href', '../Styles/style_alt.css');
   } else {
-      // Sinon, changer vers le premier
       styleFeuille.setAttribute('href', '../Styles/style.css');
   
   }  
 };
 
+//Nolan
+//On notes les chemins puis on les mets dans un tableau pour prendre au hasard après.
 function randomFiche() {
   const OursPolaire = '../Content/Collection/Ours_Polaire.xhtml';
   const HarfangDesNeiges = '../Content/Collection/Harfang_des_neiges.xhtml';
@@ -40,12 +46,15 @@ function randomFiche() {
   return ficheArray[Math.floor(Math.random() * ficheArray.length)];
 };
 
-
+//Nolan
+//On se sert de la fonction du dessus pour la concretiser
 function redirectToRandom() {
   let randomFicheUrl = randomFiche();
   window.location.href = randomFicheUrl;
 };
-  
+
+//Nolan
+//On fait simplement en sorte que l'mage soit cliquable et que ça change. currentIndex est a l'exterieur sinon on ne pourrais pas revenir sur la premiere images.
 var images = ["./Images/Image_Accueil/Image_acceuill.jpeg", "./Images/Image_Accueil/LR-oiseaux-nicheurs-PC.jpg"];
 var currentIndex = 0;
 function changeImage() {
@@ -58,7 +67,7 @@ function changeImage() {
   }
   img.src = images[currentIndex]
 };
-
+//Nolan et Elliot
 function validateAnimalName() {
   var animalNameInput = document.getElementById("Animal-name");
   var animalNameError = document.getElementById("animalNameError");
@@ -72,7 +81,7 @@ function validateAnimalName() {
     animalNameInput.classList.remove("error");
   }
 };
-
+//Nolan et Elliot
 function validateVulnerability() {
   var vulnerabilityError = document.getElementById("vulnerabilityError");
   var radios = document.querySelectorAll('input[name="Vulnerability"]');
@@ -90,13 +99,13 @@ function validateVulnerability() {
     vulnerabilityError.textContent = "";
   }
 };
-
+//Nolan et Elliot
 function validateForm() {
   validateAnimalName();
   validateVulnerability();
-  // Ajoute d'autres fonctions de validation ici si nécessaire
+  //Ajoute d'autres fonctions de validation ici si nécessaire
 
-  // Empêche la soumission du formulaire si des erreurs sont présentes
+  //Empêche la soumission du formulaire si des erreurs sont présentes
   var errors = document.querySelectorAll('.error');
   if (errors.length > 0) {
     alert("Veuillez corriger les erreurs dans le formulaire avant de le soumettre.");
@@ -104,7 +113,7 @@ function validateForm() {
     document.getElementById("myForm").submit();
   }
 };
-
+//Nolan et Elliot
 function showSummary() {
   
   var animalName = document.getElementById("Animal-name").value;
